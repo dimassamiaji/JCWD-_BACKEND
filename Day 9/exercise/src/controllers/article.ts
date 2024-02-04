1174077bae9865e7af4595331e18db5f4aea6ee0
dimassamiaji <dimassamji@gmail.com>
2024-02-05 01:18:42 +0700

    update

--- Day 9/exercise/src/controllers/article.ts
+++ Day 9/exercise/src/controllers/article.ts
@@ -1,16 +1,51 @@
 import { Response, Request, NextFunction } from "express";
+import { prisma } from "..";
 
 export const articleController = {
-  create(req: Request, res: Response, next: NextFunction) {
-    res.send("test");
+  async create(req: Request, res: Response, next: NextFunction) {
+    try {
+      await prisma.article.create({ data: req.body });
+    } catch (error) {
+      next(error);
+    }
   },
-  read(req: Request, res: Response, next: NextFunction) {
-    res.send("test");
+  async read(req: Request, res: Response, next: NextFunction) {
+    try {
+      await prisma.article.findMany({
+        select: {
+          title: true,
+          content: true,
+          user: {
+            select: {
+              first_name: true,
+              last_name: true,
+            },
+          },
+          article_categories: {
+            select: {
+              category: {
+                select: {
+                  category: true,
+                },
+              },
+            },
+          },
+        },
+      });
+    } catch (error) {
+      next(error);
+    }
   },
-  update(req: Request, res: Response, next: NextFunction) {
-    res.send("test");
+  async update(req: Request, res: Response, next: NextFunction) {
+    try {
+    } catch (error) {
+      next(error);
+    }
   },
-  delete(req: Request, res: Response, next: NextFunction) {
-    res.send("test");
+  async delete(req: Request, res: Response, next: NextFunction) {
+    try {
+    } catch (error) {
+      next(error);
+    }
   },
 };
--- Day 9/exercise/src/controllers/user.ts
+++ Day 9/exercise/src/controllers/user.ts
@@ -1,10 +1,49 @@
 import { Response, Request, NextFunction } from "express";
+import { prisma } from "..";
+import { Prisma } from "@prisma/client";
 
 export const userController = {
-  register(req: Request, res: Response, next: NextFunction) {
-    res.send("test");
+  async register(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { email, password, first_name, last_name, gender } = req.body;
+      const newUser: Prisma.UserCreateInput = {
+        email,
+        password,
+        first_name,
+        last_name,
+        gender,
+      };
+      const checkUser = await prisma.user.findMany({
+        where: {
+          email,
+        },
+      });
+
+      if (checkUser.length > 0) throw Error("user sudah terdaftar");
+
+      await prisma.user.create({
+        data: req.body,
+      });
+    } catch (error) {}
   },
-  login(req: Request, res: Response, next: NextFunction) {
-    res.send("test");
+  async login(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { email, password } = req.query;
+      const user = await prisma.user.findMany({
+        where: {
+          email: String(email),
+          password: String(password),
+        },
+      });
+
+      if (user.length == 0) throw Error("email/password salah");
+
+      res.send({
+        success: true,
+        result: user,
+      });
+    } catch (error) {
+      next(error);
+    }
   },
 };
